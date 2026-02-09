6a32ed7ed5c4841a5553450b57b6e443f94c67e6
lindkvst <lindkviste@gmail.com>
2026-02-09 15:00:46 +0100

    Opgave2

--- src/main/java/com/example/thymeleafdemo/PersonController.java
+++ src/main/java/com/example/thymeleafdemo/PersonController.java
@@ -15,7 +15,8 @@ public class PersonController {
                 new Person("John", 20),
                 new Person("Sarah", 22),
                 new Person("Simon", 23),
-                new Person("Søren", 40)
+                new Person("Søren", 40),
+                new Person("Lisa", 15)
         ));
 
 
